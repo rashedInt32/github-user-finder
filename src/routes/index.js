7b6c4d5271bba7e66427f5f6f080b2e79b355cb7
rashedInt32 <parvez08eee@gmail.com>
2019-10-21 08:57:39 +0600

    Setting up routes

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+
+function Routes() {
+  return (
+    <Router>
+      <Route path="/" component={Home} />
+    </Router>
+  )
+}
+
+export default Routes;
